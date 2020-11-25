4db085c91a1659a7b031c5c21e19bfc11e56e5b6
danielsarmientodev <danielsarmientoenterprise@gmail.com>
2020-11-24 19:33:20 -0500

    delete switch Platform

--- App.js
+++ App.js
@@ -1,11 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View,Image,Platform } from 'react-native';
-//SwitchPlatform Sirve para variar el contenido tanto en ios o en android
-const switchPlatform = Platform.select({
-ios: 'Estoy en IO',
-android:'Estoy en Android'
-});
+import { StyleSheet, Text, View,Image } from 'react-native';
+
 
 export default function App() {
   return (
@@ -16,9 +12,6 @@ export default function App() {
       />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-      <Text>
-        {switchPlatform}
-      </Text>
     </View>
   );
 }
