e0ef2f0b905b02584ba15866e6e5d87d2e542199
danielsarmientodev <danielsarmientoenterprise@gmail.com>
2020-11-24 20:44:13 -0500

    Add flex header

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Text} from 'react-native';
 import Home from './src/screens/containers/home';
 import Header from './src/sections/components/header';
+
 export default function App() {
     return (
       <Home>
@@ -9,6 +10,7 @@ export default function App() {
         <Text>Buscador</Text>
         <Text>Categorías</Text>
         <Text>Sugerencias</Text>
+      
       </Home>
       
     );
