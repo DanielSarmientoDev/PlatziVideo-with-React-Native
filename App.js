90c0eba0a8cc823b5acbd6bdfb6d1a60338d2862
danielsarmientodev <danielsarmientoenterprise@gmail.com>
2020-11-25 15:10:08 -0500

    add Loader

--- App.js
+++ App.js
@@ -1,34 +1,48 @@
 import React, { Component } from 'react';
-import {Text} from 'react-native';
+import {Text,View,StyleSheet, ActivityIndicator} from 'react-native';
 import Home from './src/screens/containers/home';
 import Header from './src/sections/components/header';
+import Loader from './src/videos/components/loader';
 import SuggestionList from './src/videos/containers/suggestions-list';
 import API from './utils/api';
 
 export default class App extends React.Component{
   state = {
-    suggestionList: []
+    suggestionList: [],
+    isLoading:true
   }
+  
   async componentDidMount(){
     const movies  = await API.getSuggestion(10);
     this.setState({
       suggestionList:movies,
+      isLoading:false
     })
   }
   render(){
-    const {suggestionList} = this.state;
+    const {suggestionList,isLoading} = this.state;
     return (
       <Home>
-        <Header />
-        <Text>Buscador</Text>
-        <Text>Categorías</Text>
-        <SuggestionList 
-        movies={suggestionList}/>
-      
+          <Header />
+          <Text>Buscador</Text>
+          <Text>Categorías</Text>
+          {
+            isLoading ?
+            <Loader color="#e4e4"/>
+            :
+          <SuggestionList movies={suggestionList}/>
+          }
+          
       </Home>
-      
     );
   }
 }
 
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:"center",
+  }
+})
 
